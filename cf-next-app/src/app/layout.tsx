b07c1c6271be77bd6d0df594368f552eab64a634
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-21 02:49:58 +0200

    Update layout.tsx

--- cf-next-app/src/app/layout.tsx
+++ cf-next-app/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -26,9 +27,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <div className="flex">
+          <Navbar />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
   );

b9645a51a012ab0a4f5906f59b9a30095c4813be
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-21 02:49:47 +0200

    Create Navbar.tsx

---
+++ cf-next-app/src/app/components/navbar/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Web from './Web'
+
+function Navbar() {
+  return (
+    <div>
+      <div>
+        <Web/>
+      </div>
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
